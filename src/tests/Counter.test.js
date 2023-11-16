14ff62ebc433dfae261ef390c4ad7b0c54662ffd
cjannun <93235878+cjannun@users.noreply.github.com>
2023-11-16 17:56:03 -0500

    Wrote 4 unit automated unit tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,35 @@
 // import necessary react testing library helpers here
+import { render, screen, fireEvent } from '@testing-library/react';
 // import the Counter component here
+import Counter from '../components/Counter.js'
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessge = screen.getByText(/Counter/i);
+  expect(counterMessge).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByText(/0/i);
+  expect(initialCount).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button', {name: '+'}));
+  const incrementedCount = screen.getByText(/1/i);
+  expect(incrementedCount).toBeInTheDocument();
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button', {name: '-'}));
+  const decrementedCount = screen.getByText(/-1/i);
+  expect(decrementedCount).toBeInTheDocument();
 });
